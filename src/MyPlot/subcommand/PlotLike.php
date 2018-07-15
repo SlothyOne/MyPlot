9f60cacff1fcd96228d41b1ce8c9903f629702a9
Ryan <ryanshaw411@gmail.com>
2018-07-15 14:46:08 +0100

    Update PlotLike.php

--- src/MyPlot/subcommand/PlotLike.php
+++ src/MyPlot/subcommand/PlotLike.php
@@ -1,1 +1,27 @@
-
+<?php
+declare(strict_types=1);
+namespace MyPlot\subcommand;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+class PlotLike extends SubCommand
+{
+	/**
+	 * @param CommandSender $sender
+	 *
+	 * @return bool
+	 */
+	public function canUse(CommandSender $sender) : bool {
+		return ($sender instanceof Player) and $sender->hasPermission("myplot.command.like");
+	}
+	/**
+	 * @param Player $sender
+	 * @param string[] $args
+	 *
+	 * @return bool
+	 */
+	public function execute(CommandSender $sender, array $args) : bool {
+  
+		return true;
+	}
+}
